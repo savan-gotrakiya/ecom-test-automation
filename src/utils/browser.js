26646362c5439409e55d5bf774a78ace12fcdd3a
Savan <savan@Savans-MacBook-Air.local>
2025-08-29 19:06:20 +0530

    feat: add playwright-chromium depedency

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,9 +1,13 @@
-import { chromium } from "playwright";
+import { chromium } from "playwright-chromium";
 import { logger } from "./logger.js";
 
 export async function launchBrowser() {
   try {
-    const browser = await chromium.launch({ headless: true });
+    // Launch headless Chromium
+    const browser = await chromium.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"], // required for serverless
+    });
     const context = await browser.newContext();
     const page = await context.newPage();
     return { browser, page };
