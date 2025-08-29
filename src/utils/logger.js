837b30cc6d34b7e88063131314a99f1883cca7ea
Savan <savan@Savans-MacBook-Air.local>
2025-08-29 19:02:12 +0530

    remove logs folder

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -11,6 +11,5 @@ export const logger = winston.createLogger({
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/app.log" }),
   ],
 });
